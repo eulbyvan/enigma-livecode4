c1e03cfd7b705c4e83badae3b1e032eb12047f8c
eulbyvan <work.issdq@gmail.com>
2022-11-24 20:02:33 +0700

    updated Match entity

--- src/main/java/com/enigmacamp/App.java
+++ src/main/java/com/enigmacamp/App.java
@@ -1,11 +1,16 @@
 package com.enigmacamp;
 
 import com.enigmacamp.constants.Position;
+import com.enigmacamp.entity.Match;
 import com.enigmacamp.entity.Player;
 import com.enigmacamp.entity.Skill;
 import com.enigmacamp.entity.Club;
+import com.enigmacamp.repo.implementation.MatchRepo;
 import com.enigmacamp.repo.implementation.PlayerRepo;
 import com.enigmacamp.repo.implementation.ClubRepo;
+import com.enigmacamp.service.implementations.ClubService;
+import com.enigmacamp.service.implementations.MatchService;
+import com.enigmacamp.service.implementations.PlayerService;
 import com.enigmacamp.utils.JpaUtil;
 import jakarta.persistence.EntityManager;
 
@@ -15,43 +20,84 @@ public class App {
     public static void main(String[] args) {
         EntityManager em = JpaUtil.getEntityManager();
         ClubRepo clubRepo = new ClubRepo(em);
+        ClubService clubService = new ClubService(clubRepo);
+
         PlayerRepo playerRepo = new PlayerRepo(em);
+        PlayerService playerService = new PlayerService(playerRepo);
 
-        //region test create team and player
-        Club club = new Club();
-        Skill skill = new Skill();
-        Player player = new Player();
+        MatchRepo matchRepo = new MatchRepo(em);
+        MatchService matchService = new MatchService(matchRepo);
 
-        club.setName("t1");
-        club.setCity("t1");
 
-        player.setName("p1");
-        player.setBirthDate(new Date());
-        player.setCitizenship("p1");
-        player.setHeight(1.80);
-        player.setPosition(Position.CenterForward);
-        skill.setAgility(7);
-        skill.setShootPower(7);
-        skill.setStrength(7);
-        skill.setReflex(7);
-        skill.setDribble(7);
-        skill.setStamina(7);
-        skill.setDetermination(7);
-        skill.setPlayer(player);
-        player.setSkill(skill);
-        player.setClub(club);
+        //region test create team, skill and player
+//        Club club = new Club();
+//        Skill skill = new Skill();
+//        Player player = new Player();
+//
+//        club.setName("c2");
+//        club.setCity("c2");
+//
+//        player.setName("p2");
+//        player.setBirthDate(new Date());
+//        player.setCitizenship("p2");
+//        player.setHeight(1.80);
+//        player.setPosition(Position.Back);
+//        skill.setAgility(7);
+//        skill.setShootPower(7);
+//        skill.setStrength(7);
+//        skill.setReflex(7);
+//        skill.setDribble(7);
+//        skill.setStamina(7);
+//        skill.setDetermination(7);
+//        skill.setPlayer(player);
+//        player.setSkill(skill);
+//        player.setClub(club);
+//
+//        club.getPlayers().add(player);
+////
+//        clubService.addClub(club);
+//
+//        playerRepo.create(player);
 
-        club.getPlayers().add(player);
+//        clubRepo.getRow(1).getPlayers().forEach(System.out::println);
+//
+        //endregion
 
-        clubRepo.create(club);
+        //region add match
+//        Match match = new Match();
+//        Club homeClub = clubService.getClub(1);
+//        Club awayClub = clubService.getClub(2);
+//
+//        match.setHomeClub(homeClub);
+//        match.setAwayClub(awayClub);
+//
+//        matchService.addMatch(match);
+//
+//        homeClub.getHomeMatches().add(match);
+//
+//        awayClub.getAwayMatches().add(match);
 
-        playerRepo.create(player);
+//        Club club1 = clubService.getClub(1);
+//        Club club2 = clubService.getClub(1);
+//
+//        System.out.println("== home matches club 1 ==");
+        clubService.getClub(1).getHomeMatches().forEach(System.out::println);
+//
+//        System.out.println("== away match club 1 ==");
+//        club1.getAwayMatches().forEach(System.out::println);
 
-//        team.getPlayers().forEach(System.out::println);
+        //endregion
 
-        clubRepo.getRow(1).getPlayers().forEach(System.out::println);
+        //region start match
+//        Match match = matchService.getMatch(1);
+//
+//        matchService.startMatch(match);
+        //endregion
+
+
+
+
 
         em.close();
-        //endregion
     }
 }
--- src/main/java/com/enigmacamp/entity/Match.java
+++ src/main/java/com/enigmacamp/entity/Match.java
@@ -3,11 +3,13 @@ package com.enigmacamp.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @NamedQuery(name = "getMatches", query = "SELECT m FROM Match m ORDER BY m.id")
 
 @Entity
 @Table(name = "t_match")
+@ToString
 public class Match {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
